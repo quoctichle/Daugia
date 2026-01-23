9fa0d9c99b6ba839e93a7c70853c8b8cc4cb7771
quoctichle <quoctichle@gmail.com>
2026-01-23 11:31:04 +0700

    Use localhost MongoDB for production

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb'
 
-const uri = process.env.MONGODB_URI || "mongodb+srv://quoctichle_db_user:Letich37@cluster0.ol5cjn6.mongodb.net/daugia?retryWrites=true&w=majority"
+const uri = process.env.MONGODB_URI || (process.env.NODE_ENV === 'production' ? "mongodb://localhost:27017" : "mongodb+srv://quoctichle_db_user:Letich37@cluster0.ol5cjn6.mongodb.net/daugia?retryWrites=true&w=majority")
 
 if (!uri) {
   throw new Error("Missing MONGODB_URI")
